ab1f45fb23a772d769e06fd0e652e64356c682cc
saker132 <saker132@gmail.com>
2023-11-16 23:55:23 -0600

    initial commit

---
+++ climb-with-me-api/src/test/java/dev/sethaker/climbwithme/ClimbWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sethaker.climbwithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
