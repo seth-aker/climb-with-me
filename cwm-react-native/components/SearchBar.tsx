bb268d1bc8366ea6fdfae383f0498180c86f7ad4
cellothere <109959547+cellothere@users.noreply.github.com>
2023-11-22 23:45:13 -0500

    merging conflicts

--- cwm-react-native/components/SearchBar.tsx
+++ cwm-react-native/components/SearchBar.tsx
@@ -1,14 +1,14 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextInput, View, StyleSheet } from 'react-native';
 
 interface SearchBarProps {
   onSearch: (query: string) => void; // Callback function to handle search
 }
 
-
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState<string>('');
+  const [placeholderIndex, setPlaceholderIndex] = useState<number>(0);
+  const placeholderTexts = ['Find activities', 'Find people', 'Search for something'];
 
   // Function to handle text input change
   const handleInputChange = (query: string) => {
@@ -20,11 +20,20 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
     onSearch(searchQuery);
   };
 
+  // Use useEffect to update placeholder text every 2 seconds
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setPlaceholderIndex((prevIndex) => (prevIndex + 1) % placeholderTexts.length);
+    }, 2000);
+
+    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+  }, [placeholderTexts.length]);
+
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.input}
-        placeholder="Search..."
+        placeholder={placeholderTexts[placeholderIndex]}
         value={searchQuery}
         onChangeText={handleInputChange}
         onSubmitEditing={handleSearch}
@@ -33,19 +42,18 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
     padding: 10,
-    width: '40%'
+    width: '90%',
   },
   input: {
     height: 40,
     borderColor: 'gray',
     borderWidth: 3,
     paddingLeft: 10,
+    borderRadius: 20,
   },
 });
-
 
 export default SearchBar;
