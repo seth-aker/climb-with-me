c36a36cf2f57f82423b2c8f061de93b76b5ecef4
cellothere <109959547+cellothere@users.noreply.github.com>
2023-11-23 00:08:13 -0500

    fixing seach bar position

--- cwm-react-native/app/(tabs)/index.tsx
+++ cwm-react-native/app/(tabs)/index.tsx
@@ -3,21 +3,21 @@ import { Text, View } from '../../components/Themed';
 import SearchBar from '../../components/SearchBar';
 import { CwmLogo } from '../../components/CwmLogo';
 
-
-
 export default function TabOneScreen() {
-  
   function handleSearch(query: string): void {
     throw new Error('Function not implemented.');
-}
-  
+  }
+
   return (
     <View style={styles.container}>
-      <SearchBar onSearch={handleSearch} /> 
-      <CwmLogo width='75px' height='75px' fill='#567944'/>
-      <Text style={styles.title}>Let's Climb!</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text>Find Climbers in your Area</Text>
+      <SearchBar onSearch={handleSearch} />
+
+      <View style={styles.contentContainer}>
+        <CwmLogo width='75px' height='75px' fill='#567944' />
+        <Text style={styles.title}>Let's Climb!</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <Text>Find Climbers in your Area</Text>
+      </View>
     </View>
   );
 }
@@ -26,12 +26,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'flex-start', // Adjusted to start from the top
+    paddingTop: 20, // Added padding to give space at the top
+  },
+  contentContainer: {
+    alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: '#567944'
+    color: '#567944',
   },
   separator: {
     marginVertical: 30,
