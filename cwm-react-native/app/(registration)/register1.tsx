49f489568d14513f923a8fd0224aa4a069c048b4
saker132 <saker132@gmail.com>
2023-11-25 18:16:41 -0700

    Adding input validation using validator.js package

--- cwm-react-native/app/(registration)/register1.tsx
+++ cwm-react-native/app/(registration)/register1.tsx
@@ -1,25 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TextInput, View, StyleSheet } from 'react-native';
 import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
-
+import { NumberFormatValues, PatternFormat } from "react-number-format"
+import validator from 'validator'
 const maxDate = new Date();
-
 export function RegisterUserInfo() {
     const [email, setEmail] = useState('');
     const [firstName, setFirstName] = useState('');
     const [middleName, setMiddleName] = useState('');
     const [lastName, setLastName] = useState('');
     const [dateOfBirth, setDateOfBirth] = useState(new Date());
-    const [primaryPhone, setPrimaryPhone] = useState(0);
+    const [primaryPhone, setPrimaryPhone] = useState('');
     const [gender, setGender] = useState('');
-
+    const [errors, setErrors] = useState<string[]>([]);
 
     const handleEmailChange = (input: string) => {
         setEmail(input);
+        if(!validator.isEmail(email) && !errors.includes("Please enter a valid email address")) {
+            setErrors(["Please enter a valid email address", ...errors])
+        }
     }
 
     const handleFirstNameChange = (input: string) => {
         setFirstName(input);
+        if(!input && !errors.includes("First name cannot be empty")) {
+            setErrors(["First name cannot be empty", ...errors])
+        }
     }
 
     const handleMiddleNameChange = (input: string) => {
@@ -37,6 +43,12 @@ export function RegisterUserInfo() {
             setDateOfBirth(date);
         }
     };
+
+    const handlePhoneNumberChange = (value: NumberFormatValues) => {
+        setPrimaryPhone(value.formattedValue)
+    }
+
+
 
     return (
         <View>
@@ -65,6 +77,14 @@ export function RegisterUserInfo() {
                 maximumDate={maxDate}
                 value={dateOfBirth}
                 onChange={handleDateOfBirthChange}
+            />
+            <PatternFormat 
+                type='tel'
+                format='+1 (###)-###-####'
+                mask="_"
+                value={primaryPhone}
+                onValueChange={handlePhoneNumberChange}
+                required
             />
         </View>
         
